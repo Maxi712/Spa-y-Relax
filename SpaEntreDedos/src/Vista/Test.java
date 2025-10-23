3a10da26b8f0901b90e3cc236496b0a9846da54f
Maximiliano Castro <maxicastro628@gmail.com>
2025-10-23 20:58:35 -0300

    Clase test con nuestros nombres y guardarlo en dase de datos

--- SpaEntreDedos/src/Vista/Test.java
+++ SpaEntreDedos/src/Vista/Test.java
@@ -22,33 +22,34 @@ public class Test {
     public static void main(String[] args) {
        Conexion conexion = new Conexion () ; 
        entidadData entidadData = new entidadData (conexion ) ; 
+       Cliente a =new Cliente(1,44738237,"Castro Maximiliano",26601927,20,"Ninguna", true);
+       entidadData.guardarCliente(a);
+       Cliente b =new Cliente(2,40238734,"Barros Edgardo",26632763,26,"Ninguna", true);
+       entidadData.guardarCliente(b);
+       Cliente c =new Cliente(3,41934744,"Carrizo Anna",266387424,24,"Ninguna", true);
+       entidadData.guardarCliente(c);
+       Cliente d =new Cliente(4,40238274,"Gimenez Pablo",266372834,23,"Ninguna", true);
+       entidadData.guardarCliente(d);
+       Cliente e =new Cliente(5,42293843,"Fernandez Rodrigo",26635233,24,"Ninguna", true);
+       entidadData.guardarCliente(e);
        
-        Cliente c2 = new Cliente();
+        ArrayList<Cliente> cli = new ArrayList();
+       cli= entidadData.listarCliente();
+       for (Cliente cliente : cli) {
+            System.out.println("Los nombres son");
+            System.out.println(cliente.getNombreCompleto());;
+        }
+       
+    }
+        /*Cliente c2 = new Cliente();
         c2.setDni(12345678);
         c2.setNombreCompleto("Edgardo Barros");
         c2.setTelefono(266498695);
         c2.setEdad(52);
         c2.setAfecciones("Ninguna");
-        c2.setEstado(false);
-       
-       
-       
-         entidadData.guardarCliente(c2);
-          
-         Cliente buscado = entidadData.buscarCliente(c2.getCodCli());
-        if (buscado != null) {
-            System.out.println("Cliente encontrado:" + buscado.getNombreCompleto());
-        }
-         ArrayList<Cliente> lista = entidadData.listarCliente();
-        System.out.println("lista de cliente");
-        for (Cliente cliente : lista) {
-            System.out.println(cliente.getCodCli() + "*" + cliente.getNombreCompleto());
-        }
-          
-                 
+        c2.setEstado(false);*/
+  
         
-      
-        
-      
-    }
 }
+    
+
