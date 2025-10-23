870a2f1ae972157fdb692cdc04b5380b05dbc876
Maximiliano Castro <maxicastro628@gmail.com>
2025-10-23 18:51:39 -0300

    Agregue el metodo listarCliente

---
+++ SpaEntreDedos/src/Vista/TestDeConsola.java
@@ -0,0 +1,14 @@
+
+package Vista;
+
+import Persistencia.entidadData;
+import java.util.ArrayList;
+import spaentrededos.Cliente;
+
+
+public class TestDeConsola {
+    public static void main(String[] args) {
+   
+
+    }
+}
